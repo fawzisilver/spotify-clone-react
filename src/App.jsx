3da7062bbf95923ca71ce8731121df79e5fffb7f
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-07-06 00:46:48 -0600

    connecting to github

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+//rfce
+import React from 'react'
+import Sidebar from './components/Sidebar.jsx'
+
+const App = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className='h-[90%] flex'>
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
+
+export default App
