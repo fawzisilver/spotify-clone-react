77ba10dbf1826659741092a525dd730e6b58718a
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-07-14 18:20:59 -0600

    added useRef and useLocation() hooks

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -8,7 +8,7 @@ const AlbumItem = ({ name, desc, image, id }) => {
    const navigate = useNavigate();
 
     return (
-        <div onClick={() => navigate(`album/${id}`)} className='min-w-[180px] p-2 px-3  cursor-pointer hover:bg-[#ffffff26]'>
+        <div onClick={() => navigate(`album/${id}`)} className='min-w-[180px] p-2 px-3 cursor-pointer hover:bg-[#ffffff26]'>
             <img className='rounded' src={image} />
             <p className='font-bold mt-2 mb-1'>{name}</p>
             <p className='text-slate-200 text-sm'>{desc}</p>
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import React, { useRef } from 'react'
+import {Route, Routes, useLocation } from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
 
 const Display = () => {
+
+  const displayRef = useRef();
+  const location = useLocation()
+  console.log(location)
+
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] ml-0'>
+    <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] ml-0'>
         <Routes>
             <Route path='/' element={<DisplayHome />}/>
             <Route path="/album/:id" element={<DisplayAlbum />} />
